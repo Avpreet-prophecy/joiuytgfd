37bf7d595a49e123473b0f814a04982899362da8
avpreet@prophecy.io <avpreet@prophecy.io>
2025-01-27 10:59:20 +0000

    Remove Aggregate_1 transformation from my_pipeline

--- pipelines/my_pipeline/code/src/main/scala/io/prophecy/pipelines/my_pipeline/main.scala
+++ pipelines/my_pipeline/code/src/main/scala/io/prophecy/pipelines/my_pipeline/main.scala
@@ -15,9 +15,8 @@ import java.time._
 object Main {
 
   def apply(context: Context): Unit = {
-    val df_Mydataset   = Mydataset(context)
-    val df_Reformat_2  = Reformat_2(context,  df_Mydataset)
-    val df_Aggregate_1 = Aggregate_1(context, df_Mydataset)
+    val df_Mydataset  = Mydataset(context)
+    val df_Reformat_2 = Reformat_2(context, df_Mydataset)
   }
 
   def main(args: Array[String]): Unit = {
